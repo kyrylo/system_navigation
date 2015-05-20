7da77a2c954aebd35eebacc85cf59bb214ed6451
Kyrylo Silin <silin@kyrylo.org>
2015-05-20 20:13:19 +0300

    unbound_method_utils: refactor duplications

--- lib/system_navigation/unbound_method_utils.rb
+++ lib/system_navigation/unbound_method_utils.rb
@@ -1,28 +1,32 @@
 class SystemNavigation
   module UnboundMethodUtils
     refine UnboundMethod do
+      def reads_field?(ivar)
+        self.with_scanner do |s|
+          s.scan_for(self.decoder_class.ivar_read_scan(_for: ivar, with: s))
+        end
+      end
+
+      def writes_field?(ivar)
+        self.with_scanner do |s|
+          s.scan_for(self.decoder_class.ivar_write_scan(_for: ivar, with: s))
+        end
+      end
+
+      def has_literal?(literal)
+        self.with_scanner do |s|
+          s.scan_for(self.decoder_class.literal_scan(_for: literal, with: s))
+        end
+      end
+
       def decoder_class
         InstructionStream::Decoder
       end
 
-      # Answer whether the receiver loads the instance variable indexed by the
-      # argument.
-      def reads_field?(ivar)
+      def with_scanner
         scanner = InstructionStream.on(self)
         scanner.decode
-        scanner.scan_for(self.decoder_class.ivar_read_scan(_for: ivar, with: scanner))
-      end
-
-      def writes_field?(ivar)
-        scanner = InstructionStream.on(self)
-        scanner.decode
-        scanner.scan_for(self.decoder_class.ivar_write_scan(_for: ivar, with: scanner))
-      end
-
-      def has_literal?(literal)
-        scanner = InstructionStream.on(self)
-        scanner.decode
-        scanner.scan_for(self.decoder_class.literal_scan(_for: literal, with: scanner))
+        yield scanner
       end
     end
   end
