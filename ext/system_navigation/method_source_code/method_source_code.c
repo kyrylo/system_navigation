d8b2dd27f5edea6304aeafbd2953a0daa006090a
Kyrylo Silin <silin@kyrylo.org>
2015-05-30 04:05:50 +0300

    Work in progress

--- ext/system_navigation/method_source_code/method_source_code.c
+++ ext/system_navigation/method_source_code/method_source_code.c
@@ -3,32 +3,44 @@
 static VALUE eSourceNotFoundError;
 
 static VALUE
+lines_for(VALUE file)
+{
+	return rb_io_s_readlines(1, file);
+}
+
+static VALUE
 mMethodExtensions_source(VALUE self)
 {
-    VALUE method = rb_iv_get(self, "@method");
-    VALUE source_location = rb_funcall(method, rb_intern("source_location"), 0);
-    VALUE name = rb_funcall(method, rb_intern("name"), 0);
+    VALUE method, source_location, name, file, line;
+
+    method = rb_iv_get(self, "@method");
+    source_location = rb_funcall(method, rb_intern("source_location"), 0);
+    name = rb_funcall(method, rb_intern("name"), 0);
 
     if (NIL_P(source_location)) {
         rb_raise(eSourceNotFoundError, "Could not locate source for %s!",
 		 RSTRING_PTR(rb_sym2str(name)));
     }
 
-    return name;
+    file = RSTRING_PTR(RARRAY_AREF(source_location, 0));
+    line = RARRAY_AREF(source_location, 1);
+
+    return lines_for(file);
+    /* return expression_at(lines_for(file), line); */
 }
 
 void Init_method_source_code(void)
 {
-    VALUE cSystemNavigation = rb_define_class("SystemNavigation", rb_cObject);
-    VALUE mMethodSourceCode = rb_define_module_under(cSystemNavigation,
-						     "MethodSourceCode");
+    VALUE cSystemNavigation, mMethodSourceCode, mMethodExtensions;
+
+    cSystemNavigation = rb_define_class("SystemNavigation", rb_cObject);
+    mMethodSourceCode = rb_define_module_under(cSystemNavigation, "MethodSourceCode");
 
     eSourceNotFoundError = rb_define_class_under(mMethodSourceCode,
 						 "SourceNotFoundError",
 						 rb_eStandardError);
 
-    VALUE mMethodExtensions = rb_define_module_under(mMethodSourceCode,
-						     "MethodExtensions");
+    mMethodExtensions = rb_define_module_under(mMethodSourceCode, "MethodExtensions");
 
     rb_define_method(mMethodExtensions, "source", mMethodExtensions_source, 0);
 }
